1c0284708004a970c2c2e9a5f567191b3a3cbf3f
Ritesh <railsriteshc56@gmail.com>
2020-12-12 00:25:10 +0530

    Configure Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user! , unless: :root_controller?
+
+  def after_sign_in_path_for
+    root_path
+  end
+
+  def after_sign_out_path_for
+    root_path
+  end
+
+  protected
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+  # end
+
+  def root_controller?
+    controller_name == root_path.split("/").first
+  end
 end
