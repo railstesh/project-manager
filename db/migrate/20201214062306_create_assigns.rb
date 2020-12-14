ec0d9b30746c41ad484c026cf64405b2c888577e
Ritesh <railsriteshc56@gmail.com>
2020-12-14 11:56:37 +0530

    Create assign model

---
+++ db/migrate/20201214062306_create_assigns.rb
@@ -0,0 +1,9 @@
+class CreateAssigns < ActiveRecord::Migration[5.2]
+  def change
+    create_table :assigns do |t|
+      t.string :assigned
+      t.integer :billing_hours
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_14_061752) do
+ActiveRecord::Schema.define(version: 2020_12_14_062306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "assigns", force: :cascade do |t|
+    t.string "assigned"
+    t.integer "billing_hours"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
