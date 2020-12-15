aef54aadfe036e73b6e99e31368cf3dccdeea525
Ritesh <railsriteshc56@gmail.com>
2020-12-15 11:43:30 +0530

    Add fields in project

---
+++ db/migrate/20201215060307_add_fields_in_project.rb
@@ -0,0 +1,11 @@
+class AddFieldsInProject < ActiveRecord::Migration[5.2]
+  def change
+    add_column :projects, :profile, :string
+    add_column :projects, :work_limit, :integer
+    add_column :projects, :technology, :string
+    add_column :projects, :invoice_type, :integer, default: 0
+    add_column :projects, :status, :integer, default: 0
+    add_column :projects, :active_discription, :string
+    add_column :projects, :inactive_discription, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_14_062729) do
+ActiveRecord::Schema.define(version: 2020_12_15_060307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 2020_12_14_062729) do
     t.string "client_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "profile"
+    t.integer "work_limit"
+    t.string "technology"
+    t.integer "invoice_type", default: 0
+    t.integer "status", default: 0
+    t.string "active_discription"
+    t.string "inactive_discription"
   end
 
   create_table "users", force: :cascade do |t|
