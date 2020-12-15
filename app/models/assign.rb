c31ab8b93e2d9cd7f2c74422da3ad8bde561a934
Ritesh <railsriteshc56@gmail.com>
2020-12-15 18:38:13 +0530

    Add enum in assign model

--- app/models/assign.rb
+++ app/models/assign.rb
@@ -2,4 +2,7 @@ class Assign < ApplicationRecord
   belongs_to :project
   belongs_to :user
   belongs_to :employee
+
+  enum assigned_as: { Trainee: 0, Developer: 1, Helping_hand: 2, Lead: 3, Manager: 4 }
+  enum status: { Active: 0, Inactive: 1 }
 end
