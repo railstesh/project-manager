1adcaf5f2cec0d71e6c845c42307c143b2121bf0
Ritesh <railsriteshc56@gmail.com>
2020-12-21 13:04:36 +0530

    Add association

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,9 +3,10 @@ class Project < ApplicationRecord
   
   has_many :assigns
   has_many :descriptions, as: :descriptable
+  belongs_to :profile
+  belongs_to :technology
 
   enum invoice_type: { Weekly: 0, Monthly: 1 }
-  enum status: { Active: 0, Inactive: 1 }
 
   accepts_nested_attributes_for :assigns, reject_if: :all_blank, allow_destroy: true
 end
