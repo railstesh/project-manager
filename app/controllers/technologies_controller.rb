0b0432bd1c9d5c020f717c1b5f0574067d106824
Ritesh <railsriteshc56@gmail.com>
2020-12-21 11:39:40 +0530

    Create technology controller

---
+++ app/controllers/technologies_controller.rb
@@ -0,0 +1,47 @@
+class TechnologiesController < ApplicationController
+  before_action :load_technology, only: %i[edit update destroy show]
+
+  def index
+    @technologies = Technology.all
+  end
+
+  def new
+    @technology = Technology.new
+  end
+
+  def create
+    @technology = Technology.new(technology_params) 
+
+    if @technology.save
+      redirect_to technologies_path
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @technology.update(technology_params)
+      redirect_to technologies_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @technology.destroy
+  end
+
+  def show; end
+
+  protected
+
+  def technology_params
+    params.require(:technology).permit(:name)
+  end
+
+  def load_technology
+    @technology = Technology.find(params[:id])
+  end
+end
