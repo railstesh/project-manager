b63b48939a82f2127916ad2e225f7346e018a72d
Ritesh <railsriteshc56@gmail.com>
2020-12-22 15:20:02 +0530

    Remove unused enum

--- app/models/assign.rb
+++ app/models/assign.rb
@@ -6,5 +6,4 @@ class Assign < ApplicationRecord
   belongs_to :employee
 
   enum assigned_as: { Trainee: 0, Developer: 1, Helping_hand: 2, Lead: 3, Manager: 4 }
-  enum status: { Active: 0, Inactive: 1 }
 end
