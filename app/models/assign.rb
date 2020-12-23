4ed9578ca846b63941314f4c2124916222aa89c5
Ritesh <railsriteshc56@gmail.com>
2020-12-23 14:21:15 +0530

    Added validation

--- app/models/assign.rb
+++ app/models/assign.rb
@@ -6,4 +6,6 @@ class Assign < ApplicationRecord
   belongs_to :employee
 
   enum assigned_as: { Trainee: 0, Developer: 1, Helping_hand: 2, Lead: 3, Manager: 4 }
+
+  validates_presence_of :project_id, :employee_id, :billing_hours, message: "This field shouldn't be blank"
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -8,4 +8,6 @@ class Project < ApplicationRecord
   enum invoice_type: { Weekly: 0, Monthly: 1 }
 
   accepts_nested_attributes_for :assigns, reject_if: :all_blank, allow_destroy: true
+
+  validates_presence_of :title, :client_name, :profile_id, :work_limit, :technologies, message: "This field shouldn't be blank", on: :create
 end
