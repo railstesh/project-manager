f5221778322b5c1aa15cf8dc49f6e101cc32da6a
Ritesh <railsriteshc56@gmail.com>
2020-12-28 13:20:58 +0530

    Calculate monthyear

--- app/models/assign.rb
+++ app/models/assign.rb
@@ -8,4 +8,16 @@ class Assign < ApplicationRecord
   enum assigned_as: { Trainee: 0, Developer: 1, Helping_hand: 2, Lead: 3, Manager: 4 }
 
   validates_presence_of :project_id, :employee_id, :billing_hours, message: "This field shouldn't be blank"
+
+  def calculate_time
+    date1 = self.created_at.to_date
+    date2 = DateTime.now.to_date
+    date = date_diff(date1, date2)
+    "#{date.first} years, #{date.second} months"
+  end
+
+  def date_diff(date1, date2)
+    month = (date2.year * 12 + date2.month) - (date1.year * 12 + date1.month)
+    month.divmod(12)
+  end
 end
