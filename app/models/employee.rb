03121ebb6056a899ebe9d0778c5088b81c4e7b1b
Ritesh <railsriteshc56@gmail.com>
2021-01-06 17:27:07 +0530

    Add employee name with status

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -8,4 +8,8 @@ class Employee < ApplicationRecord
 
   validates :name, presence: true, uniqueness: {case_sensitive: false}
   validates :email, presence: true, uniqueness: true, format: { with: Devise::email_regexp } 
+
+  def name_with_status
+    "#{name} (#{status})"
+  end
 end
