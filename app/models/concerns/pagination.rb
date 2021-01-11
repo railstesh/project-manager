66df303effcba2012f09500db43054972a88afe6
Ritesh <railsriteshc56@gmail.com>
2021-01-11 17:20:03 +0530

    Added class variable

--- app/models/concerns/pagination.rb
+++ app/models/concerns/pagination.rb
@@ -1,7 +1,9 @@
 module Pagination
   include ActiveSupport::Concern
 
+  @@per_page = 9
+
   def processed_pagination(page_no)
-    paginate(page: page_no, per_page: 9)
+    paginate(page: page_no, per_page: @@per_page)
   end
 end
